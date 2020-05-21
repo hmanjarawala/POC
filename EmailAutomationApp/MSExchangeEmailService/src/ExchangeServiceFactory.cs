6a89f1a77f2771aafcd6c00f0d605e28e1f98229
Himanshu M <hmanjarawala@gmail.com>
2020-05-21 15:03:56 +0530

    Add new exchangeServerFactory class to Repository

---
+++ EmailAutomationApp/MSExchangeEmailService/src/ExchangeServiceFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Com.Service.Email.ExchangeServer
+{
+    public class ExchangeServiceFactory
+    {
+        public static IMSExchangeService GetExchangeWebClient(string username, string password)
+        {
+            return new MSExchangeService(username, password);
+        }
+
+        public static IMSExchangeService GetExchangeClient(string username, string password, string domain)
+        {
+            return new MSExchangeService(username, password, domain);
+        }
+    }
+}
