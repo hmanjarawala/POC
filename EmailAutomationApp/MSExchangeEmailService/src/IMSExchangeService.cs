c83bad9753ef6797f3be50601c513d3b57831f38
Himanshu M <hmanjarawala@gmail.com>
2020-05-21 17:48:50 +0530

    Added 2 new Methods 1) GetFolders 2) ReadEmail in Interface

--- EmailAutomationApp/MSExchangeEmailService/src/IMSExchangeService.cs
+++ EmailAutomationApp/MSExchangeEmailService/src/IMSExchangeService.cs
@@ -1,14 +1,14 @@
 ﻿using Microsoft.Exchange.WebServices.Data;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Com.Service.Email.ExchangeServer
 {
     public interface IMSExchangeService
     {
         IEnumerable<EmailMessage> ReadEmails(string folderName = "Inbox");
+
+        EmailMessage ReadEmail(ItemId id);
+
+        IEnumerable<Folder> GetFolders();
     }
 }
