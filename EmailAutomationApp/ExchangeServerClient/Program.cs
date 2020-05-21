a98fafa6a5f9b7cabac41f5816e6897418a4ac5e
Himanshu M <hmanjarawala@gmail.com>
2020-05-21 15:05:48 +0530

    Commit ExchangeServerClient Project to repository

---
+++ EmailAutomationApp/ExchangeServerClient/Program.cs
@@ -0,0 +1,19 @@
+﻿using Com.Service.Email.ExchangeServer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExchangeServerClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IMSExchangeService service = ExchangeServiceFactory.GetExchangeClient("himanshu.manjarawala", "a0dF@theR9", "MEA");
+
+            var mails = service.ReadEmails();
+        }
+    }
+}
