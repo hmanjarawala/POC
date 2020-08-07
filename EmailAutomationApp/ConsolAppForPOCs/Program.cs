e7ef0cdb34db5c1680f875d1ecce5d42dde44149
Himanshu M <hmanjarawala@gmail.com>
2020-08-07 14:37:29 +0530

    Changes and Error Solving

--- EmailAutomationApp/ConsolAppForPOCs/Program.cs
+++ EmailAutomationApp/ConsolAppForPOCs/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using ConsolAppForPOCs.VisionAPI.ocr;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +12,13 @@ namespace ConsolAppForPOCs
     {
         static void Main(string[] args)
         {
+            string workingDirectory = Environment.CurrentDirectory;
+            string projectDirectory = Directory.GetParent(workingDirectory).Parent.FullName;
+            string imageFilePath = projectDirectory + @"\Files\Image1.jpg";
+
+            var extractText = new ExtractPrintedText();
+
+            extractText.StartExtraction(imageFilePath).Wait();
         }
     }
 }
