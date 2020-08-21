e4964d58b42c983f9927191ef9387f7886bb0ca0
Himanshu M <hmanjarawala@gmail.com>
2020-08-21 14:45:29 +0530

    Added Startup class in Rest Api

---
+++ RestAPISample/AspNetWebApiRest/Startup.cs
@@ -0,0 +1,29 @@
+﻿using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace AspNetWebApiRest
+{
+    public class Startup
+    {
+        // Creates an OWIN pipeline for hosting Web Api
+        // and configures the routing.
+        public void Configuration(IAppBuilder app)
+        {
+            var config = new HttpConfiguration();
+
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi", 
+                routeTemplate: "api/{controller}/{id}", 
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            app.UseWebApi(config);
+        }
+    }
+}
