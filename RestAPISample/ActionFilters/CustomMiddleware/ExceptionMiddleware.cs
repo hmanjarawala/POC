fe969d58e0effee2348a2f259f6b4005d62294b1
Himanshu M <hmanjarawala@gmail.com>
2020-08-24 18:05:05 +0530

    Added ExceptionMiddleware class in ActionFilters.CustomMiddleware namespace

---
+++ RestAPISample/ActionFilters/CustomMiddleware/ExceptionMiddleware.cs
@@ -0,0 +1,44 @@
+﻿using ActionFilters.Entities;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace ActionFilters.CustomMiddleware
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(httpContext, ex);
+            }
+        }
+
+        private static Task HandleExceptionAsync(HttpContext httpContext, Exception exception)
+        {
+            httpContext.Response.ContentType = "application/json";
+            httpContext.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+            return httpContext.Response.WriteAsync(new ErrorDetails()
+            {
+                StatusCode = httpContext.Response.StatusCode,
+                Message = "Internal Server Error from the custom middleware."
+            }.ToString());
+        }
+    }
+}
