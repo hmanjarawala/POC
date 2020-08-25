507588d4f5c7d877afc505b4e1603208afe2dd44
Himanshu M <hmanjarawala@gmail.com>
2020-08-25 13:43:51 +0530

    Added ValidationFilterAttribute class in ActionFilters.ActionFilters namespace

---
+++ RestAPISample/ActionFilters/ActionFilters/ValidationFilterAttribute.cs
@@ -0,0 +1,27 @@
+﻿using ActionFilters.Contracts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Linq;
+
+namespace ActionFilters.ActionFilters
+{
+    public class ValidationFilterAttribute : IActionFilter
+    {
+        public void OnActionExecuted(ActionExecutedContext context) { }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            var param = context.ActionArguments.SingleOrDefault(x => x.Value is IEntity);
+            if(param.Value == null)
+            {
+                context.Result = new BadRequestObjectResult("Object is null");
+                return;
+            }
+
+            if (!context.ModelState.IsValid)
+            {
+                context.Result = new BadRequestObjectResult(context.ModelState);
+            }
+        }
+    }
+}
