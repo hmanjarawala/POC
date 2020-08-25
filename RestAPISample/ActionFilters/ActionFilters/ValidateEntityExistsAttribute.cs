fe14c2d7d52e6e73e34b193714591f7bb6cca368
Himanshu M <hmanjarawala@gmail.com>
2020-08-25 13:40:50 +0530

    Removed unneccessary using statements

--- RestAPISample/ActionFilters/ActionFilters/ValidateEntityExistsAttribute.cs
+++ RestAPISample/ActionFilters/ActionFilters/ValidateEntityExistsAttribute.cs
@@ -3,9 +3,7 @@ using ActionFilters.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace ActionFilters.ActionFilters
 {
